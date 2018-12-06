4eae103d6cdeb620c20288d5047f7c29533e7db0
ChenYingYu <alan831007@gmail.com>
2018-12-06 20:22:20 +0800

    Initial Commit

---
+++ DynamicChart/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DynamicChart
+//
+//  Created by ChenAlan on 2018/12/6.
+//  Copyright © 2018年 ChenAlan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
